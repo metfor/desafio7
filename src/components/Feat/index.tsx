e00a5091390767b2a4040b9e5c9f87066cbcf128
metfor <mateus_frade@hotmail.com>
2021-11-17 16:32:20 -0300

    fix: correções

--- src/components/Feat/index.tsx
+++ src/components/Feat/index.tsx
@@ -1,55 +1,55 @@
-import { Box, BoxData, BoxSocialNetwork, Data, FooterContainer, SocialNetwork, Year } from "./styled";
+import {
+  Box,
+  BoxData,
+  BoxSocialNetwork,
+  Data,
+  FooterContainer,
+  Line,
+  SocialNetwork,
+  Year,
+} from "./styled";
 
-export function Footer (){
-    return(
-        <FooterContainer>
-            <BoxSocialNetwork>
-            <Box>
-                <SocialNetwork
-                    className="intagram"
-                    src="/../public/instagram.png"
-                    alt="istagram"
-                    width="20"
-                    height="20"
-                
-                />
-                </Box>
-                <Box>
-                <SocialNetwork
-                    className="twitter"
-                    src="/../public/twitter.png"
-                    alt="twitter"
-                    width="20"
-                    height="20"
-                
-                />
-                </Box>
-                <Box>
-                <SocialNetwork
-                    className="facebook"
-                    src="/../public/facebook.png"
-                    alt="facebook"
-                    width="20"
-                    height="20"
-                
-                />
-                </Box>
-            </BoxSocialNetwork>
-            <BoxData>
-                <Data>
-                    29<br/>
-                    08
-                </Data>
-                <SocialNetwork
-                   className="facebook"
-                   src="/../public/line.png"
-                   alt="facebook"
-                   width="2"
-                   height="146" 
-                
-               / >
-                   <Year>2021</Year>
-            </BoxData>
-        </FooterContainer>
-    )
+export function Footer() {
+  return (
+    <FooterContainer>
+      <BoxSocialNetwork>
+        <Box>
+          <SocialNetwork
+            className="intagram"
+            src="/../public/instagram.png"
+            alt="istagram"
+            width="20"
+            height="20"
+          />
+        </Box>
+        <Box>
+          <SocialNetwork
+            className="twitter"
+            src="/../public/twitter.png"
+            alt="twitter"
+            width="20"
+            height="20"
+          />
+        </Box>
+        <Box>
+          <SocialNetwork
+            className="facebook"
+            src="/../public/facebook.png"
+            alt="facebook"
+            width="20"
+            height="20"
+          />
+        </Box>
+      </BoxSocialNetwork>
+      <BoxData>
+        <Data>
+          29
+          <br />
+          08
+        </Data>
+        <Line />
+        <Year>2021</Year>
+      </BoxData>
+    </FooterContainer>
+  );
 }
--- src/components/HeaderV/index.tsx
+++ src/components/HeaderV/index.tsx
@@ -1,29 +1,34 @@
-import { BoxImg, BoxShare, ContainerHeader, ImgShare, LogoImg, Riot } from "./styled";
+import {
+  BoxImg,
+  BoxShare,
+  ContainerHeader,
+  ImgShare,
+  LogoImg,
+  Riot,
+} from "./styled";
 
-export function HeaderV(){
-    return(
-        <ContainerHeader>
-        <BoxImg>
-            <LogoImg
-               className="logo"
-               src="/../public/logo.svg"
-               alt="logo"
-               width="58"
-               height="39" 
-               
-            />
-           
-        </BoxImg>
-        <Riot>Riot Games</Riot>
-        <BoxShare>
-            <ImgShare
-                className="logo"
-                src="/../public/share.svg"
-                alt="logo"
-                width="25"
-                height="25" 
-            />
-        </BoxShare>
+export function HeaderV() {
+  return (
+    <ContainerHeader>
+      <BoxImg>
+        <LogoImg
+          className="logo"
+          src="/../public/logo.svg"
+          alt="logo"
+          width="58"
+          height="39"
+        />
+      </BoxImg>
+      <Riot>Riot Games</Riot>
+      <BoxShare>
+        <ImgShare
+          className="logo"
+          src="/../public/share.svg"
+          alt="logo"
+          width="25"
+          height="25"
+        />
+      </BoxShare>
     </ContainerHeader>
-    )
+  );
 }
